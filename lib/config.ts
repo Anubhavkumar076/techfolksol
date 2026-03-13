079c7a841b72011c202319f657c68e88bac1b776
Anubhavkumar076 <37979259+Anubhavkumar076@users.noreply.github.com>
2026-03-14 03:48:16 +0530

    qwerty

--- lib/config.ts
+++ lib/config.ts
@@ -14,7 +14,5 @@
 // Example: "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgVE2upms"
 export const GOOGLE_SHEET_ID = "1QGrVc2tq-9AIcMfG8TXTXkeYIg7Qst3bmz_0-Kcb40I";
 
-// Step 2: After deploying your Apps Script as a Web App, paste the full URL here.
-// It will look like: https://script.google.com/macros/s/AKfycb.../exec
-// NOTE: This URL is issued by Apps Script and is separate from GOOGLE_SHEET_ID.
+
 export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwSQY4w7g8_dI1ju0ZtnEyNrQAlDccdlUxqIPW_jVHuzvi9LJW4WqCEnHBRWk3Qs6OYDA/exec";
